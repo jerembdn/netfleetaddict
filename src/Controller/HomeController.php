d3b7c5420b7777599efcf7a1bc4bfef38df6af32
Jérémy <work@jeremybdn.fr>
2023-02-01 16:29:28 +0100

    💄 add a little bit of content inside homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\SheetService;
 
+#[Route('/', name: 'home')]
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    private SheetService $sheetService;
+
+    public function __construct(SheetService $sheetService)
+    {
+        $this->sheetService = $sheetService;
+    }
+    
+    #[Route('/', name: '_index')]
     public function index(): Response
     {
+        $sheets = $this->sheetService->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'sheets' => $sheets
         ]);
     }
 }
